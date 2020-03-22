16695ffe0d3f6aa38c2b955cc0c4c98cf5e779ab
Kaicheng Jia <ares.jia@hotmail.com>
2020-03-22 19:45:48 +0800

    heroku

--- server/server.js
+++ server/server.js
@@ -11,12 +11,14 @@ require('dotenv').config();
 
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.MONGODB_URI)
 
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static('client/build'))
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -528,6 +530,13 @@ app.post('/api/users/update_profile',auth,(req,res)=>{
 
 
 
+// DEFAULT 
+if( process.env.NODE_ENV === 'production' ){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 
 
